0b8075016b6c9f363793d5932cbdb50955ffd926
Adrianna-IO <isom.adrianna@gmail.com>
2020-04-06 15:15:24 -0400

    pushing easy challenge

---
+++ JavaScriptChallenges-WEEK3/Javascript/easy.js
@@ -0,0 +1,26 @@
+// EASY: Write a function that would allow you to do this:
+// var run = exercise('running');
+// console.log(run()); // prints "Today's exercise: running"
+// var swim = exercise('swimming');
+// console.log(swim()); // prints "Today's exercise: swimming"
+
+
+function exercise(sport) {
+    return (function() {
+        return `Today's excercise: ${sport}!`
+    })
+}
+
+var run = exercise('running');
+console.log(run()); 
+
+
+
+function exercise(sport) {
+    return (function() {
+        return `Today's excercise: ${sport}!`
+    })
+}
+
+var swim = exercise('swimming');
+console.log(swim()); 
