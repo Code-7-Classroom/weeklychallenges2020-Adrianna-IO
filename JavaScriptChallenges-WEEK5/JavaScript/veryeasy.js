163f142a9345aa045e88019f7dc33c9f51694b1d
Adrianna-IO <isom.adrianna@gmail.com>
2020-04-16 16:01:46 -0400

    pushed up very easy and file structure

---
+++ JavaScriptChallenges-WEEK5/JavaScript/veryeasy.js
@@ -0,0 +1,11 @@
+// VERY EASY: What is the difference between the following 
+// JavaScript keywords: var, let and const.  Please provide 
+// a written explanation. You are able to include a code 
+// reference along with your written explanation for each keyword.
+
+// var is not block scoped, which means it can be accessed (and mutated) in or outside of a block or {}. Also,
+//  var doesn't have to be assigned a variable when declared unlike 'let' and 'const'.
+
+// let is block scoped, which means if it is declared inside a block it can NOT be accessed (or mutated) outside of the block.
+
+// const is also a block scoped variable, it defines a constant REFERENCE to a value. The primitive values can NOT be mutuated. 
