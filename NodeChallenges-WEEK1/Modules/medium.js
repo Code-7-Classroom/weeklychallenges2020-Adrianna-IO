b6c8e19ff42d727afe02f5287fa9048a4be79f6e
Adrianna-IO <isom.adrianna@gmail.com>
2020-04-23 21:50:22 -0400

    pushed up medium challenge

---
+++ NodeChallenges-WEEK1/Modules/medium.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+
+const solarSystem = fs.readFile('../solarSystem.rtf', function(err, filetext) {
+    if (err) {
+        console.log(`Error: ${err}`)
+    } else {
+        console.log(`File Output: ${filetext}`)
+    }
+});
+console.log(solarSystem);
